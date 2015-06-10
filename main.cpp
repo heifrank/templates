6b18e758b23df64765502c6938c7e6735b359acf
heifrank <heifrank27@163.com>
2015-06-11 05:04:23 +0800

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <queue>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+#include <unordered_map>
+using namespace std;
+
+#define PB push_back
+#define MP make_pair
+#define SIZE(v) ((int)(v).size())
+#define CLEAR(v) memset((v), 0, sizeof(v))
+#define SORT(v) sort((v).begin(), (v).end())
+#define FOR(i, l, r) for(int i=(l); i<(r); i++)
+#define FOREACH(i, v) for(__typeof((v).begin()) i=(v).begin(); i!=(v).end(); i++)
+const int INF = 1001001001;
+const double PI = 2 * acos(0.0);
+typedef unsigned uint;
+typedef long long ll;
+typedef vector<int> vint;
+typedef vector<string> vstr;
+typedef pair<int, int> pii;
+template<typename T> void pv(T a, T b){ for(T i = a; i != b; ++i)cout << *i << " "; cout << endl; }
+template<typename T> void chmin(T &t, const T &f){ if(t > f) t = f; }
+template<typename T> void chmax(T &t, const T &f){ if(t < f) t = f; }
+
+
+int main(){
+	    return 0;
+}
+
